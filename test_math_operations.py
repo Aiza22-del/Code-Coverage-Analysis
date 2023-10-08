0f88cd9b7398729276a825734eb1279348779579
Aiza <you@example.com>
2023-10-08 22:17:07 +0500

    first commit

---
+++ test_math_operations.py
@@ -0,0 +1,19 @@
+import math_operations
+
+def test_addition():
+    assert math_operations.add(2, 2) == 4
+
+def test_subtraction():
+    assert math_operations.subtract(5, 3) == 2
+
+def test_multiplication():
+    assert math_operations.multiply(4, 3) == 12
+
+def test_division():
+    assert math_operations.divide(10, 2) == 5
+    try:
+        math_operations.divide(5, 0)
+    except ZeroDivisionError:
+        assert True
+    else:
+        assert False
